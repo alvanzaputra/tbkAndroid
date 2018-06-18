a2bfe7082d50ca082606454dc2a7449bd27b8189
alvanzaputra <alvanzaputraa@gmail.com>
2018-06-18 07:41:34 +0700

    Initial commit

---
+++ app/src/main/java/id/tabook/dev/tabook/TravelOrderActivity.kt
@@ -0,0 +1,12 @@
+package id.tabook.dev.tabook
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TravelOrderActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_travel_order)
+    }
+}
